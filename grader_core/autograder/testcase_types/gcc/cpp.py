064338277cd1a516a695e92178b3c52307445a71
Oluwadamilola Oyenekan <adeniyioyenekan@yahoo.co.uk>
2023-07-24 20:48:56 +0100

    Add grading system core application

---
+++ grader_core/autograder/testcase_types/gcc/cpp.py
@@ -0,0 +1,11 @@
+from grader_core.autograder.testcase_types.gcc.c import TestCase as CTestCase
+from grader_core.autograder.testcase_utils.shell import get_shell_command
+
+
+class TestCase(CTestCase):
+    source_suffix = ".cpp"
+    compiler = get_shell_command("g++")
+
+    @classmethod
+    def get_template_dir(cls):
+        return cls.type_source_file.parent / "c++_templates"
